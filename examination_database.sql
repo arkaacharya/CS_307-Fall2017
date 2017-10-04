30d73618b19ab33c2c25109311cc7e2efe4bac31
heyixiong <he284@purdue.edu>
2017-10-04 19:49:46 -0400

    Updated examination_database
    Added exam and course tables. Check course_owner with teacher_id to see who owns the course. Check course_id's in course table and exam table to see which exam is in which course.

--- examination_database.sql
+++ examination_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 02, 2017 at 07:10 AM
+-- Generation Time: Oct 05, 2017 at 01:48 AM
 -- Server version: 10.1.26-MariaDB
 -- PHP Version: 7.1.9
 
@@ -21,6 +21,30 @@ SET time_zone = "+00:00";
 --
 -- Database: `examination`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `course_id` int(20) NOT NULL,
+  `course_owner` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `exam`
+--
+
+CREATE TABLE `exam` (
+  `exam_id` int(20) NOT NULL,
+  `exam_name` varchar(20) DEFAULT NULL,
+  `time_limit` int(5) DEFAULT NULL,
+  `course_id` int(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -62,6 +86,18 @@ CREATE TABLE `user_teacher` (
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`course_id`);
+
+--
+-- Indexes for table `exam`
+--
+ALTER TABLE `exam`
+  ADD PRIMARY KEY (`exam_id`);
 
 --
 -- Indexes for table `user`
