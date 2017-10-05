ad1ba02951dd7fa454e7aa5e11b95675eca518ae
dleemelee <dleemelee@users.noreply.github.com>
2017-10-04 20:51:38 -0400

    Completed login frontend
    Added register slot with tabs, along with logo on top

--- login.php
+++ login.php
@@ -1,34 +1,43 @@
 <html>
   <head>
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="css/login.css" type="text/css">
-  </head>
-  <body>
     <script language="javascript" type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script language="javascript" type="text/javascript" src="js/login.js"></script>
-    <div id="Student" class="w3-display-container">
-      <span onclick="this.parentElement.style.display=''none" class="w3-button w3-display-topright"></span>
+  </head>
+  <body>
+
+    <div id="header">
+      <div id="header-wrapper">
+        <div id="header">
+          <div id="logo">
+        <h1><a href="#">Exami-Nation</a></h1>
+        <p>[Logo goes here]</p>
+      </div>
+    </div>
+  </div>
+    </div>
+
+
       <div class="login-page">
-        <div class="form">
-          <!--
+        <div class="form" id="form">
           <form class="register-form">
             <input type="text" placeholder="name"/>
             <input type="password" placeholder="password"/>
             <input type="text" placeholder="email address"/>
             <button>create</button>
             <p class="message">Already registered? <a href="#">Sign In</a></p>
-          </form> 
-          -->
+          </form>
+
           <form class="login-form"
-          action = "validateLogin.php"
-          method = "post">
-            <input type="text" placeholder="username" name="username"/>
-            <input type="password" placeholder="password" name="password"/>
-            <button>login</button>
+            action = "validateLogin.php"
+            method = "post">
+            <input type="text" placeholder="username" name="username" size="20" maxlength="20"/>
+            <input type="password" placeholder="password" name="password"  size="20" maxlength="20"/>
+            <button id="login">login</button>
             <p class="message">Not registered? <a href="#">Create an account</a></p>
           </form>
         </div>
       </div>
-    </div>
+    
   </body>
 </html>
